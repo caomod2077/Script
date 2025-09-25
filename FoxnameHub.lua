0ef0623afa87f231c7270645c0fe54d7250dc473
Caomod <116502323+caomod2077@users.noreply.github.com>
2025-09-26 00:37:18 +0700

    Update FoxnameHub.lua

--- FoxnameHub.lua
+++ FoxnameHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/dd211535cf41416f91e2b42e77693ba8"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/506fbf24444c4fa38389800b78ebb81c"))()
