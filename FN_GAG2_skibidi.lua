976377516dc852e77f95d49546bf4177b1007cca
Caomod <116502323+caomod2077@users.noreply.github.com>
2026-06-16 00:39:45 +0700

    Update FN_GAG2_skibidi.lua

--- FN_GAG2_skibidi.lua
+++ FN_GAG2_skibidi.lua
@@ -1,1 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/ccc8664d5d3679f2cd4175a92e12a0670466d69fbcf9a342d05c9dfa002f1669/download"))()
