3319f5af13dc4f3eed643dd8331eb63684d5df84
AmongUs <128269328+tanhoangviet@users.noreply.github.com>
2026-07-16 19:13:32 +0700

    Update script URL in FN_AnimalHospital.lua

--- FN_AnimalHospital.lua
+++ FN_AnimalHospital.lua
@@ -1,3 +1,3 @@
 -- Luraph is cooking lol W junkie
 getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/fa665c8adbbf23869fd19578b2ae7d0c872e3c1e8d8930ae37e4dc882d0c88d6/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2c204a401a9911f52c2b3bb51ab4c6c10dc8c78d1234cf27bdbb8c94b3f87e13/download"))()
