817fe13c0f5c99589127c9c98e3ba231e3dd6c90
afkar <afkargantengbetjir@gmail.com>
2026-07-22 08:57:29 +0700

    Update FN_AnimalHospital.lua

--- FN_AnimalHospital.lua
+++ FN_AnimalHospital.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/0f22fff95d034a7d91744b663474d9229c52751b/FN_AnimalHospital.lua"))()  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/6249021824297f3915248dd801eeef378ee244c4/FN_AnimalHospital.lua"))()  
